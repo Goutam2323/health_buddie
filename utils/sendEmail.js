604c31bc1dbba5337c862fe5d6cb58b7491767ce
Goutam2323 <bishtgoutam3@gmail.com>
2024-10-27 12:16:51 +0530

    set email pass

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,33 +1,33 @@
-const nodemailer = require('nodemailer');
-
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 const sendEmail = async (options, callback) => {
-    try {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'bishtgoutam3@gmail.com', // your Gmail address
-                pass: 'nawi kxfp etfz oaqt' // your Gmail password or App Password
-            }
-        });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "bishtgoutam3@gmail.com", // your Gmail address
+        pass: process.env.Email, // your Gmail password or App Password
+      },
+    });
 
-        const mailOptions = {
-            from: 'bishtgoutam3@gmail.com', // sender address
-            to: options.email, // receiver email
-            subject: options.subject,
-            text: options.message
-        };
+    const mailOptions = {
+      from: "bishtgoutam3@gmail.com", // sender address
+      to: options.email, // receiver email
+      subject: options.subject,
+      text: options.message,
+    };
 
-        await transporter.sendMail(mailOptions);
-        console.log('Email sent successfully');
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
 
-        // If a callback is provided, invoke it
-        if (callback) {
-            callback(null);
-        }
-    } catch (error) {
-        console.error('Error sending email:', error);
-        throw new Error('Error sending OTP');
+    // If a callback is provided, invoke it
+    if (callback) {
+      callback(null);
     }
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Error sending OTP");
+  }
 };
 
 module.exports = sendEmail;
